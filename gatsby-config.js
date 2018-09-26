07f6bd01d5f2133450c833deffba00a3107f9d99
sw-yx <swyx@swyx.io>
2018-09-26 11:02:52 -0400

    switch to md from mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     {
       resolve: `gatsby-mdx`,
       options: {
+        extensions: [".mdx", ".md"],
         gatsbyRemarkPlugins: [
           {
             resolve: "gatsby-remark-images",
